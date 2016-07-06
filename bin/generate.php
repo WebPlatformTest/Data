ec29b5a44b9fb4ed895b2e73e4c773f0cb68db56
Niels Leenheer <niels@leenheer.nl>
2016-07-06 15:47:06 +0200

    Match the new updated table structure on the website

--- bin/generate.php
+++ bin/generate.php
@@ -4,40 +4,40 @@ $data = [];
 
 $variants = json_decode(file_get_contents(__DIR__ . '/../data/platforms.json'));
 
-$data[] = "TRUNCATE browserVariants;";
+$data[] = "TRUNCATE data_platforms;";
 foreach ($variants as $key => $value) {
-    $data[] = "INSERT INTO browserVariants SET " .
-        "`id`='" . addslashes($value->platform) . "', " .
-        "`replaced`=" . (!empty($value->related) ? "'" . addslashes($value->related) . "'" : 'NULL') . ", " .
+    $data[] = "INSERT INTO data_platforms SET " .
+        "`platform`='" . addslashes($value->platform) . "', " .
+        "`related`=" . (!empty($value->related) ? "'" . addslashes($value->related) . "'" : 'NULL') . ", " .
         "`name`='" . addslashes($value->name) . "', " .
         "`grouped`='" . addslashes($value->grouped) . "', " .
-        "`importance`=" . intval($value->order) . ", " .
+        "`order`=" . intval($value->order) . ", " .
         "`type`='" . addslashes($value->type) . "';";
 }
 
 
 $versions = json_decode(file_get_contents(__DIR__ . '/../data/versions.json'));
 
-$data[] = "TRUNCATE browserVersions;";
+$data[] = "TRUNCATE data_versions;";
 foreach ($versions as $key => $value) {
-    $data[] = "INSERT INTO browserVersions SET " .
-        "`variant`='" . addslashes($value->platform) . "', " .
+    $data[] = "INSERT INTO data_versions SET " .
+        "`platform`='" . addslashes($value->platform) . "', " .
         "`version`='" . addslashes($value->version) . "', " .
         "`nickname`='" . addslashes($value->nickname) . "', " .
         "`details`=" . (!empty($value->details) ? "'" . addslashes($value->details) . "'" : 'NULL') . ", " .
         "`release`=" . (!empty($value->release) ? "'" . addslashes($value->release) . "'" : 'NULL') . ", " .
         "`type`='" . addslashes($value->type) . "', " .
         "`status`='" . addslashes($value->status) . "', " .
-        "`listed`=" . intval($value->visible) . ";";
+        "`visible`=" . intval($value->visible) . ";";
 }
 
 
 $identifiers = json_decode(file_get_contents(__DIR__ . '/../data/tests.json'));
 
-$data[] = "TRUNCATE browserIdentifier;";
+$data[] = "TRUNCATE data_tests;";
 foreach ($identifiers as $key => $value) {
-    $data[] = "INSERT INTO browserIdentifier SET " .
-        "`variant`='" . addslashes($value->platform) . "', " .
+    $data[] = "INSERT INTO data_tests SET " .
+        "`platform`='" . addslashes($value->platform) . "', " .
         "`version`='" . addslashes($value->version) . "', " .
         "`uniqueid`=" . (!empty($value->uniqueid) ? "'" . addslashes($value->uniqueid) . "'" : 'NULL') . ", " .
         "`identifier`=" . (!empty($value->identifier) ? "'" . addslashes($value->identifier) . "'" : 'NULL') . ", " .
@@ -45,8 +45,8 @@ foreach ($identifiers as $key => $value) {
 }
 
 
-$data[] = "REPLACE INTO scores (`release`, `variant`, `version`, `fingerprint`) " .
-    "SELECT r.release, i.variant, i.version, r.fingerprint FROM browserIdentifier as i LEFT JOIN results AS r ON ((i.uniqueid IS NULL AND i.identifier = r.identifier AND i.source = r.source) OR i.uniqueid = r.uniqueid) HAVING fingerprint IS NOT NULL";
+$data[] = "REPLACE INTO scores (`release`, `platform`, `version`, `fingerprint`) " .
+    "SELECT r.release, t.platform, t.version, r.fingerprint FROM data_tests as t LEFT JOIN results AS r ON ((t.uniqueid IS NULL AND t.identifier = r.identifier AND t.source = r.source) OR t.uniqueid = r.uniqueid) HAVING fingerprint IS NOT NULL";
 
 
 echo implode("\n", $data) . "\n";
