e73aa17f2f7c46961114b53eb5b25bd270943bfa
Niels Leenheer <niels@leenheer.nl>
2016-07-10 20:30:30 +0200

    Fix another mistake in the final query

--- bin/generate.php
+++ bin/generate.php
@@ -49,7 +49,7 @@ foreach ($identifiers as $key => $value) {
 $data[] = "REPLACE INTO scores (`release`, `platform`, `version`, `fingerprint`) " .
     "SELECT r.release, t.platform, t.version, r.fingerprint " .
     "FROM data_tests as t " .
-    "LEFT JOIN results AS r ON (" +
+    "LEFT JOIN results AS r ON (" .
         "(t.uniqueid IS NULL AND t.identifier = r.identifier AND t.source = r.source) OR " .
         "(t.uniqueid IS NULL AND r.source = 'lab' AND t.useragent = r.useragent) OR " .
         "t.uniqueid = r.uniqueid" .
